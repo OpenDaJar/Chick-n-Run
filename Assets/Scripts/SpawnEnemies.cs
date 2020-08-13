8601cd57ffba3f76b490984ebee2a143f35ebae5
b10z <nikos_georg.15793@yahoo.gr>
2020-08-13 16:44:39 +0300

    Unity Implementation
    version_0.01

---
+++ Assets/Scripts/SpawnEnemies.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemies : MonoBehaviour
+{
+    public float maxTime = 1;
+    private float timer = 0;
+    public GameObject enemy;
+    public float height;
+    public Camera MainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject newpipe = Instantiate(enemy);
+        newpipe.transform.position = transform.position + new Vector3(0, height, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer > maxTime)
+        {
+            GameObject newpipe = Instantiate(enemy);
+            newpipe.transform.position = new Vector3(MainCamera.transform.position.x + MainCamera.pixelWidth / 47, -2.3f, transform.position.z);
+            Destroy(newpipe, 15);
+            timer = 0;
+        }
+        timer += Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/TerrainSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainSpawner : MonoBehaviour
+{
+
+    public Camera MainCamera;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {     
+        transform.position = new Vector3(MainCamera.transform.position.x + MainCamera.pixelWidth / 47, -4.3f, transform.position.z);
+    }
+}
